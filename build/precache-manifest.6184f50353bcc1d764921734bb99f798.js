2e531458a61caad8c96fba8da2d5b343fd6401da
Addison Wanlass <addmanrcace@gmail.com>
2020-10-19 23:22:49 -0400

    Restructured & seperated the backend

---
+++ build/precache-manifest.6184f50353bcc1d764921734bb99f798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cb9f11f8ce5badf10a0de69ab62dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc0a4bc797425b6d8403",
+    "url": "/static/css/main.2e9d3f44.chunk.css"
+  },
+  {
+    "revision": "e18254caa4ca793ab4f3",
+    "url": "/static/js/2.b1f28f8d.chunk.js"
+  },
+  {
+    "revision": "a876c180ae3716b988d9dc43b418efe7",
+    "url": "/static/js/2.b1f28f8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0a4bc797425b6d8403",
+    "url": "/static/js/main.f4bd3396.chunk.js"
+  },
+  {
+    "revision": "82dd574c5c961e49e202",
+    "url": "/static/js/runtime-main.65dbde5b.js"
+  }
+]);
